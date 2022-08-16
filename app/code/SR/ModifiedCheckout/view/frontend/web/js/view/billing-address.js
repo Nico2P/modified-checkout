c2d3704ce75278a1035bdafa4cd69dadfd0502f5
Nico <nico2p@protonmail.com>
2022-08-16 14:15:14 +0200

    Convert Select to Div with button like delivery

--- app/code/SR/ModifiedCheckout/view/frontend/web/js/view/billing-address.js
+++ app/code/SR/ModifiedCheckout/view/frontend/web/js/view/billing-address.js
@@ -62,6 +62,7 @@ define(
             currentBillingAddress: quote.billingAddress,
             addressOptions: addressOptions,
             customerHasAddresses: addressOptions.length > 1,
+            test_pouet:1,
 
             /**
              * Init component
@@ -80,7 +81,8 @@ define(
                         isAddressFormVisible: false,
                         isAddressSameAsShipping: true,
                         saveInAddressBook: 1,
-                        isAddressFormListVisible:false
+                        isAddressFormListVisible:false,
+                        btnIsAllReadyCreate:false
                     });
 
                 return this;
@@ -102,6 +104,7 @@ define(
              * @return {Boolean}
              */
             useShippingAddress: function () {
+
                 if (this.isAddressSameAsShipping()) {
                     this.isAddressFormVisible(false);
                     this.isAddressFormListVisible(false);
@@ -110,6 +113,68 @@ define(
                         this.isAddressFormVisible(true);
                     } else {
                         this.isAddressFormListVisible(true);
+
+                        //Controle btn déjà crée
+                        if (!this.btnIsAllReadyCreate()){
+
+                            for (let i = 0; i < addressOptions.length; i++) {
+                                //Loop addresse, création btn et insert dans le DOM
+                                let bill = document.getElementsByClassName('checkout-billing-address')[0];
+                                let div_addressBillingBlock = document.getElementsByClassName('addressBillingBlock')[0];
+                                let id_address;
+                                let div = document.createElement('div');
+
+                                if (i === 3){
+                                    id_address = null;
+                                } else {
+                                    id_address = addressOptions[i].customerAddressId;
+                                    div.classList.add('shipping-address-item');
+                                    div.classList.add('billing-address-item');
+                                }
+
+                                div_addressBillingBlock.append(div);
+
+                                if (id_address !== null){
+                                    // Création elements
+                                    let info_address = document.createElement("div");
+                                    let civilite = document.createElement('p');
+                                    let street = document.createElement('p');
+                                    let city = document.createElement('p');
+                                    let country_cust = document.createElement('p');
+                                    let phone = addressOptions[i].telephone;
+                                    // Bind info to elements
+                                    civilite.innerText = addressOptions[i].firstname + ' ' + addressOptions[i].lastname;
+                                    street.innerText = addressOptions[i].street;
+                                    city.innerText = addressOptions[i].city + ', ' + addressOptions[i].postcode;
+                                    country_cust.innerText = "France";
+                                    //Insert elements
+                                    info_address.append(civilite,street,city,country_cust,phone);
+                                    div.append(info_address);
+                                }
+
+                                //Création des boutons
+                                let btn = document.createElement("button");
+                                if (id_address === null){ // Si aucun ID c'est le btn de création d'adresse
+                                    btn.innerHTML = " +  Nouvelle adresse";
+                                    btn.style.float = 'none';
+                                } else {
+                                    btn.innerHTML = "Facturer à cette adresse";
+                                }
+                                //Bind Function OnClick
+                                btn.id = id_address;
+                                btn.setAttribute('onclick', 'setCustomBillingAddress('+id_address+');');
+                                btn.classList.add('action-select-shipping-item');
+                                div.append(btn);
+
+                                if (i === 0){
+                                    //Trigger la première addresse comme choix
+                                    let btn = document.getElementById(id_address);
+                                    btn.click();
+                                }
+                            }
+                            //Creation des btn ok
+                            this.btnIsAllReadyCreate(true);
+                        }
                     }
                 }
                 return true;
